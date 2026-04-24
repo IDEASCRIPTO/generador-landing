d2a5a98e743baf76f5e52208eaf472447ae0cb75
IDEASCRIPTO <omerjoao777@gmail.com>
2026-04-24 18:42:05 -0500

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json');
 
@@ -14,6 +14,7 @@ curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 120);
 $response = curl_exec($ch);
 curl_close($ch);
 echo $response;
