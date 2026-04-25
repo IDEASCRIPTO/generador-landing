2ec6c87a8069b3d8620a5512e244e77bc9a97967
IDEASCRIPTO <omerjoao777@gmail.com>
2026-04-24 20:01:58 -0500

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -16,17 +16,25 @@ if (empty($data)) {
     exit(0);
 }
 
+set_time_limit(300);
+
 $ch = curl_init('https://duallegacy-ia-asistentes-n8n.aigmej.easypanel.host/webhook/generar-landing');
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_TIMEOUT, 180);
+curl_setopt($ch, CURLOPT_TIMEOUT, 300);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 $response = curl_exec($ch);
+$error = curl_error($ch);
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
+if ($error) {
+    header('Content-Type: application/json');
+    echo json_encode(['error' => $error, 'code' => $httpCode]);
+    exit(0);
+}
+
 header('Content-Type: application/json');
-header('X-HTTP-Code: ' . $httpCode);
 echo $response;
